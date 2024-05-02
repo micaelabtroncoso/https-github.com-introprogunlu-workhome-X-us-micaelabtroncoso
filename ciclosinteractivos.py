590ac33fd84b956578af3688baab2d9273eb8d34
micaelabtroncoso <165312487+micaelabtroncoso@users.noreply.github.com>
2024-05-02 19:47:46 -0300

    Update ciclosinteractivos.py

--- ciclosinteractivos.py
+++ ciclosinteractivos.py
@@ -1,3 +1,7 @@
 def muchos_pcn (x):
    if x > 0:
-resultado numero
+resultado numero = "numero_positivo"
+  elif x ""0:
+resultado = "igual a 0"
+else resultado = "numero_negativo"
+return resultado
